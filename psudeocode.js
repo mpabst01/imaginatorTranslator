787202e9d72e350d2c95cafccb50f452bf544da5
Nilou Mostofi <nilou@km-direct.com>
2020-09-23 11:42:55 -0700

    psuedocode

---
+++ psudeocode.js
@@ -0,0 +1,19 @@
+// PSEUDO-CODE
+// main page
+// setup html with framework--header, describing quiz
+// two buttons with two choices--light and dark side
+// click function takes user to second page
+// ===========================================================================
+// second page light side with multiple-choice questions and answers
+// background image 
+// start quiz with button, one question at a time
+// if user answer correctly, display as pop-up positive affirmation gif
+// user needs to click on close button of popup
+// if user answers incorrectly, display as pop-up negative affirmation gif
+// user needs to click on close button of popup
+// user continues with questions up to the last question 7
+// user clicks on button for final results
+// ===========================================================================
+// third page dark side display score and img
+// ===========================================================================
+// fourth page display score and img
