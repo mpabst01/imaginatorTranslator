7f6cba2301ef2e0d7663d4f780e47db537b36549
Marylee Pabst <marylee@uconn.edu>
2020-09-25 15:11:29 -0700

    new files to store questions to aid simplicity

---
+++ light-questions.js
@@ -0,0 +1,68 @@
+var questions = [
+    {
+        question: "What planet does C-3PO call home?",
+        choiceA: "Tattooine",
+        choiceB: "Alderaan",
+        choiceC: "Bespin",
+        choiceD: "Naboo",
+        correctAnswer: "Tattoine",
+        triviaURL: "https://swapi.dev/api/planets/1/"
+    },
+    {
+        question: "When was Obi-Wan Kenobi born?",
+        choiceA: "57BBY",
+        choiceB: "52BBY",
+        choiceC: "102BBY",
+        choiceD: "204BBY",
+        correctAnswer: "57BBY",
+        triviaURL: "https://swapi.dev/api/people/10/"
+    },
+    {
+        question: "What planet does Yoda seek refuge from the Dark Side?",
+        choiceA: "unknown",
+        choiceB: "Dagobah",
+        choiceC: "Yavin IV",
+        choiceD: "Kamino",
+        correctAnswer: "Dagobah",
+        triviaURL: "https://swapi.dev/api/planets/5/"
+    },
+    {
+        question: "What color are Lando Calrissian's eyes?",
+        choiceA: "black",
+        choiceB: "hazel",
+        choiceC: "brown",
+        choiceD: "blue",
+        correctAnswer: "brown",
+        triviaURL: "https://swapi.dev/api/people/25/"
+    },
+    {
+            //q4
+        question: "Who is Bail Prestor Organa?",
+        choiceA: "Luke's stepfather",
+        choiceB: "Han Solo's father",
+        choiceC: "A rebel fighter", 
+        choiceD: "Leia's stepfather",
+        correctAnswer: "Leia's stepfather",
+        triviaURL: "https://swapi.dev/api/people/68/"
+    },
+    {
+        //q5
+        question: "What language does Chewbacca speak?",
+        choiceA: "Toydarian",
+        choiceB: "Neimodian",
+        choiceC: "Shyriiwook",
+        choiceD: "Huttese",
+        correctAnswer: "Shyriiwook",
+        triviaURL: "https://swapi.dev/api/species/3/"
+    },
+    {
+        //q6
+        question: "Who piloted the Millenium Falcon?",
+        choiceA: "Rey",
+        choiceB: "Lando Calrissian",
+        choiceC: "Han Solo",
+        choiceD: "All of the above",
+        correctAnswer: "All of the above",
+        triviaURL: "https://swapi.dev/api/transport/10/"
+    }
+];
