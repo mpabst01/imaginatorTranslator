8cec7b6cfb36136eee7953961cddc4f74199356b
Nilou Mostofi <nilou@km-direct.com>
2020-09-27 14:21:37 -0700

    hookup modal js to light-quiz.html

--- modal.js
+++ modal.js
@@ -1,34 +1,40 @@
-const showModal = (ev) => {
-  ev.preventDefault();
-  let modal = document.querySelector(".modal");
-  modal.classList.remove("off");
-  modal.classList.add("on");
-};
-const showOverlay = (ev) => {
-  ev.preventDefault();
-  let overlay = document.querySelector(".overlay");
-  overlay.classList.remove("hide");
-  overlay.classList.add("show");
-  showModal(ev);
-};
-const hideModal = (ev) => {
+const modal = {
+  showModal: function (ev) {
+    ev.preventDefault();
     let modal = document.querySelector(".modal");
-    modal.classList.remove ("on");
-    modal. classList.add("off");
-};
-const hideOverlay = (ev) => {
-  ev.preventDefault();
-  ev.stopPropagation();
-  let overlay = document.querySelector(".overlay");
-  overlay.classList.remove("show");
-  overlay.classList.add("hide");
-  hideModal(ev);
+    modal.classList.remove("off");
+    modal.classList.add("on");
+  },
+  showOverlay: function (ev) {
+    ev.preventDefault();
+    let overlay = document.querySelector(".overlay");
+    overlay.classList.remove("hide");
+    overlay.classList.add("show");
+    this.showModal(ev);
+  },
+  hideModal: function (ev) {
+    let modal = document.querySelector(".modal");
+    modal.classList.remove("on");
+    modal.classList.add("off");
+  },
+  hideOverlay: function (ev) {
+    ev.preventDefault();
+    ev.stopPropagation();
+    let overlay = document.querySelector(".overlay");
+    overlay.classList.remove("show");
+    overlay.classList.add("hide");
+    this.hideModal(ev);
+  },
+  init: function (ev) {
+    document.querySelector("p").addEventListener("click", function (event) {
+      modal.showOverlay(event);
+    });
+    document
+      .querySelector(".close-btn")
+      .addEventListener("click", function (ev) {
+        modal.hideOverlay(ev);
+      });
+  },
 };
 
-const init = (ev)=> {
-  document.querySelector("p").addEventListener("click", showOverlay);
-
-  document.querySelector(".close-btn").addEventListener("click", hideOverlay);
-};
-
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", () => modal.init());
