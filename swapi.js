eed39c2dad9703fef3f8878d3901ca6c92b98d2a
Nilou Mostofi <nilou@km-direct.com>
2020-09-27 15:28:55 -0700

    Make ajax call to swapi and show data in the modal for planets

--- swapi.js
+++ swapi.js
@@ -2,10 +2,11 @@
 $(document).ready(function (event) {
   var testPerson = "https://swapi.dev/api/people/44/";
   var testSpecies = "https://swapi.dev/api/species/21/";
-  var testStarships = "https://swapi.dev/api/starships/10/";
+  var testPlanets = "https://swapi.dev/api/planets/21/";
 
-    fetchTriviaFromSwapi(testPerson);
-//   fetchTriviaFromSwapi(testSpecies);
+  // fetchTriviaFromSwapi(testPerson);
+  //   fetchTriviaFromSwapi(testSpecies);
+  fetchTriviaFromSwapi(testPlanets);
 });
 
 // === end test code ===
@@ -19,21 +20,19 @@ function fetchTriviaFromSwapi(triviaURL) {
     console.log(swapiData);
 
     /* 
-    average_height: "180"
-    average_lifespan: "70"
-    classification: "amphibian"
-    created: "2014-12-20T10:18:58.610000Z"
-    designation: "sentient"
-    edited: "2014-12-20T21:36:42.181000Z"
-    eye_colors: "black"
-    films: ["http://swapi.dev/api/films/4/"]
-    hair_colors: "none"
-    homeworld: "http://swapi.dev/api/planets/44/"
-    language: "Nautila"
-    name: "Nautolan"
-    people: ["http://swapi.dev/api/people/53/"]
-    skin_colors: "green, blue, brown, red"
-    url: "http://swapi.dev/api/species/21/"
+    climate: "polluted"
+created: "2014-12-10T16:26:54.384000Z"
+diameter: "13490"
+edited: "2014-12-20T20:58:18.454000Z"
+films: []
+gravity: "1 standard"
+name: "Eriadu"
+orbital_period: "360"
+population: "22000000000"
+residents: ["http://swapi.dev/api/people/12/"]
+rotation_period: "24"
+surface_water: "unknown"
+terrain: "cityscape"
     */
 
     // create a ul
@@ -51,15 +50,24 @@ function fetchTriviaFromSwapi(triviaURL) {
       li = $("<li>").text("Birth Year: " + swapiData.birth_year);
       list.append(li);
     } else if (triviaURL.includes("species")) {
-      // language, average_lifespan, eye_colors
-      li = $("<li>").text("Language: " + swapiData.language);
-      list.append(li);
-
-      li = $("<li>").text("Avg. Life: " + swapiData.average_lifespan);
-      list.append(li);
-
-      li = $("<li>").text("Eye Colors: " + swapiData.eye_colors);
-      list.append(li);
+        li = $("<li>").text("Language: " + swapiData.language);
+        list.append(li);
+        
+        li = $("<li>").text("Avg. Life: " + swapiData.average_lifespan);
+        list.append(li);
+        
+        li = $("<li>").text("Eye Colors: " + swapiData.eye_colors);
+        list.append(li);
+    } else if (triviaURL.includes("planets")) {
+        // climate, terrain, rotation_period
+        li = $("<li>").text("Climate: " + swapiData.climate);
+        list.append(li);
+        
+        li = $("<li>").text("Terrain: " + swapiData.terrain);
+        list.append(li);
+        
+        li = $("<li>").text("Rotation Period: " + swapiData.rotation_period);
+        list.append(li);
     }
 
     // TODO (later issue): send request to get the homeworld and show it
